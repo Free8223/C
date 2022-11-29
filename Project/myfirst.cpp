59683178891505dc82411415bbced3d4d54059c5
GitPyz <Frei23@QQ.com>
2022-11-29 11:53:49 +0800

    first commit

---
+++ Project/myfirst.cpp
@@ -0,0 +1,13 @@
+// myfirst.cpp -- difsplays a message
+
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't rgret it!" << endl;
+	return 0;
+
+
+}
