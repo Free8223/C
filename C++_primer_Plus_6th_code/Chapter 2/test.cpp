593e094917941c898be0d81d3813980401e9978d
GitPyz <Frei23@QQ.com>
2022-12-02 10:15:31 +0800

    modified:   C++_primer_Plus_6th_code/Chapter 2/test.cpp 	deleted:    Project/.vs/Project/v17/.suo 	deleted:    Project/.vs/Project/v17/Browse.VC.db 	deleted:    Project/.vs/Project/v17/ipch/AutoPCH/5d19e5f186057d2e/MYFIRST.ipch 	deleted:    Project/.vs/Project/v17/ipch/AutoPCH/877f7459bedcface/GETINFO.ipch 	deleted:    Project/.vs/Project/v17/ipch/AutoPCH/cb00b54f61cc2e83/CARROT.ipch 	deleted:    Project/Project.sln 	deleted:    Project/Project.vcxproj 	deleted:    Project/Project.vcxproj.filters 	deleted:    Project/Project.vcxproj.user 	deleted:    Project/carrot.cpp 	deleted:    Project/getinfo.cpp 	deleted:    Project/myfirst.cpp 	deleted:    Project/x64/Debug/CodeAnalysisResultManifest.txt 	deleted:    Project/x64/Debug/Project.Build.CppClean.log 	deleted:    Project/x64/Debug/Project.exe 	deleted:    Project/x64/Debug/Project.exe.lastcodeanalysissucceeded 	deleted:    Project/x64/Debug/Project.exe.recipe 	deleted:    Project/x64/Debug/Project.ilk 	deleted:    Project/x64/Debug/Project.log 	deleted:    Project/x64/Debug/Project.pdb 	deleted:    Project/x64/Debug/Project.tlog/CL.command.1.tlog 	deleted:    Project/x64/Debug/Project.tlog/CL.read.1.tlog 	deleted:    Project/x64/Debug/Project.tlog/CL.write.1.tlog 	deleted:    Project/x64/Debug/Project.tlog/Project.lastbuildstate 	deleted:    Project/x64/Debug/Project.tlog/link.command.1.tlog 	deleted:    Project/x64/Debug/Project.tlog/link.read.1.tlog 	deleted:    Project/x64/Debug/Project.tlog/link.write.1.tlog 	deleted:    Project/x64/Debug/Project.vcxproj.FileListAbsolute.txt 	deleted:    Project/x64/Debug/carrot.obj 	deleted:    Project/x64/Debug/getinfo.obj 	deleted:    Project/x64/Debug/vc143.idb 	deleted:    Project/x64/Debug/vc143.pdb

--- C++_primer_Plus_6th_code/Chapter 2/test.cpp
+++ C++_primer_Plus_6th_code/Chapter 2/test.cpp
@@ -2,8 +2,12 @@
 using   namespace   std;
 int main() 
 {   
-cout << "Come up and C++ me some time.";    
-cout <<endl; 
-cout << "You won't regret it!" << endl;
-return 1; 
+	/*cout << 'M';
+	cout << "M";
+	cout.put('M');
+	cout << endl;*/
+	cout << "Come up and C++ me some time.";    
+	cout <<endl; 
+	cout << "You won't regret it!" << endl;
+	return 1; 
 }
